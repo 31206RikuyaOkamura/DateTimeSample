904dd2f596d2b7f184c9cb1152d4c5ae11cd59b7
RikuyaOkamura <31206@ojs.ac.jp>
2020-08-18 14:56:51 +0900

    プロジェクト ファイルを追加します。

---
+++ DateTimeSample/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace DateTimeSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btNow = new System.Windows.Forms.Button();
+            this.tbNowDisp = new System.Windows.Forms.TextBox();
+            this.nudDayBeforeAfter = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btBefore = new System.Windows.Forms.Button();
+            this.btAfter = new System.Windows.Forms.Button();
+            this.tbBeforeAfterDisp = new System.Windows.Forms.TextBox();
+            this.nudMonthBeforeAfter = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbLeapYear = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nudMonth = new System.Windows.Forms.NumericUpDown();
+            this.nudDay = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.nudYear = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbAns = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDayBeforeAfter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMonthBeforeAfter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMonth)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btNow
+            // 
+            this.btNow.Location = new System.Drawing.Point(24, 54);
+            this.btNow.Name = "btNow";
+            this.btNow.Size = new System.Drawing.Size(129, 39);
+            this.btNow.TabIndex = 0;
+            this.btNow.Text = "現在日時";
+            this.btNow.UseVisualStyleBackColor = true;
+            this.btNow.Click += new System.EventHandler(this.btNow_Click);
+            // 
+            // tbNowDisp
+            // 
+            this.tbNowDisp.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbNowDisp.Location = new System.Drawing.Point(159, 54);
+            this.tbNowDisp.Name = "tbNowDisp";
+            this.tbNowDisp.Size = new System.Drawing.Size(564, 39);
+            this.tbNowDisp.TabIndex = 1;
+            // 
+            // nudDayBeforeAfter
+            // 
+            this.nudDayBeforeAfter.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nudDayBeforeAfter.Location = new System.Drawing.Point(348, 127);
+            this.nudDayBeforeAfter.Name = "nudDayBeforeAfter";
+            this.nudDayBeforeAfter.Size = new System.Drawing.Size(111, 39);
+            this.nudDayBeforeAfter.TabIndex = 2;
+            this.nudDayBeforeAfter.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(465, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "日";
+            // 
+            // btBefore
+            // 
+            this.btBefore.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btBefore.Location = new System.Drawing.Point(159, 204);
+            this.btBefore.Name = "btBefore";
+            this.btBefore.Size = new System.Drawing.Size(74, 46);
+            this.btBefore.TabIndex = 4;
+            this.btBefore.Text = "前";
+            this.btBefore.UseVisualStyleBackColor = true;
+            this.btBefore.Click += new System.EventHandler(this.btBefore_Click);
+            // 
+            // btAfter
+            // 
+            this.btAfter.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btAfter.Location = new System.Drawing.Point(249, 204);
+            this.btAfter.Name = "btAfter";
+            this.btAfter.Size = new System.Drawing.Size(74, 46);
+            this.btAfter.TabIndex = 4;
+            this.btAfter.Text = "後";
+            this.btAfter.UseVisualStyleBackColor = true;
+            this.btAfter.Click += new System.EventHandler(this.btAfter_Click);
+            // 
+            // tbBeforeAfterDisp
+            // 
+            this.tbBeforeAfterDisp.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbBeforeAfterDisp.Location = new System.Drawing.Point(159, 294);
+            this.tbBeforeAfterDisp.Name = "tbBeforeAfterDisp";
+            this.tbBeforeAfterDisp.Size = new System.Drawing.Size(564, 39);
+            this.tbBeforeAfterDisp.TabIndex = 1;
+            // 
+            // nudMonthBeforeAfter
+            // 
+            this.nudMonthBeforeAfter.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nudMonthBeforeAfter.Location = new System.Drawing.Point(159, 127);
+            this.nudMonthBeforeAfter.Name = "nudMonthBeforeAfter";
+            this.nudMonthBeforeAfter.Size = new System.Drawing.Size(111, 39);
+            this.nudMonthBeforeAfter.TabIndex = 2;
+            this.nudMonthBeforeAfter.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(276, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 33);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ヵ月";
+            // 
+            // tbLeapYear
+            // 
+            this.tbLeapYear.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbLeapYear.Location = new System.Drawing.Point(159, 339);
+            this.tbLeapYear.Name = "tbLeapYear";
+            this.tbLeapYear.Size = new System.Drawing.Size(564, 39);
+            this.tbLeapYear.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(10, 342);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 33);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "閏年判定";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(79, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 33);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "西暦";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label6.Location = new System.Drawing.Point(282, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 33);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "年";
+            // 
+            // nudMonth
+            // 
+            this.nudMonth.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nudMonth.Location = new System.Drawing.Point(351, 34);
+            this.nudMonth.Maximum = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.nudMonth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudMonth.Name = "nudMonth";
+            this.nudMonth.Size = new System.Drawing.Size(78, 39);
+            this.nudMonth.TabIndex = 2;
+            this.nudMonth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudMonth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // nudDay
+            // 
+            this.nudDay.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nudDay.Location = new System.Drawing.Point(506, 34);
+            this.nudDay.Maximum = new decimal(new int[] {
+            31,
+            0,
+            0,
+            0});
+            this.nudDay.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDay.Name = "nudDay";
+            this.nudDay.Size = new System.Drawing.Size(78, 39);
+            this.nudDay.TabIndex = 2;
+            this.nudDay.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudDay.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label7.Location = new System.Drawing.Point(435, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 33);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "月";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label8.Location = new System.Drawing.Point(590, 36);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 33);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "日";
+            // 
+            // nudYear
+            // 
+            this.nudYear.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.nudYear.Location = new System.Drawing.Point(165, 34);
+            this.nudYear.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
+            this.nudYear.Name = "nudYear";
+            this.nudYear.Size = new System.Drawing.Size(111, 39);
+            this.nudYear.TabIndex = 2;
+            this.nudYear.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.nudYear.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(8, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "日数計算";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label9.Location = new System.Drawing.Point(159, 104);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(172, 33);
+            this.label9.TabIndex = 3;
+            this.label9.Text = "生まれてから";
+            // 
+            // tbAns
+            // 
+            this.tbAns.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tbAns.Location = new System.Drawing.Point(337, 101);
+            this.tbAns.Name = "tbAns";
+            this.tbAns.Size = new System.Drawing.Size(181, 39);
+            this.tbAns.TabIndex = 1;
+            this.tbAns.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label10.Location = new System.Drawing.Point(524, 104);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(153, 33);
+            this.label10.TabIndex = 3;
+            this.label10.Text = "日目です。";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.tbAns);
+            this.groupBox1.Controls.Add(this.nudYear);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.nudMonth);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.nudDay);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.groupBox1.Location = new System.Drawing.Point(16, 418);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(758, 170);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "誕生日";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 616);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btAfter);
+            this.Controls.Add(this.btBefore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudMonthBeforeAfter);
+            this.Controls.Add(this.nudDayBeforeAfter);
+            this.Controls.Add(this.tbLeapYear);
+            this.Controls.Add(this.tbBeforeAfterDisp);
+            this.Controls.Add(this.tbNowDisp);
+            this.Controls.Add(this.btNow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudDayBeforeAfter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMonthBeforeAfter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudMonth)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudYear)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btNow;
+        private System.Windows.Forms.TextBox tbNowDisp;
+        private System.Windows.Forms.NumericUpDown nudDayBeforeAfter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btBefore;
+        private System.Windows.Forms.Button btAfter;
+        private System.Windows.Forms.TextBox tbBeforeAfterDisp;
+        private System.Windows.Forms.NumericUpDown nudMonthBeforeAfter;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbLeapYear;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown nudMonth;
+        private System.Windows.Forms.NumericUpDown nudDay;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown nudYear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbAns;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ DateTimeSample/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DateTimeSample
+{
+    public partial class Form1 : Form
+    {
+        //現在時刻取得
+        private DateTime _now;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btNow_Click(object sender, EventArgs e)
+        {
+            //現在日時取得
+            _now = DateTime.Now;
+
+            tbNowDisp.Text = _now.ToString("F");
+
+#if true
+            //参考演算子
+            tbLeapYear.Text = DateTime.IsLeapYear(_now.Year) ? "閏年です。" : "閏年ではありません。";
+
+#else
+            if (DateTime.IsLeapYear(day.Year))
+            {
+                tbLeapYear.Text = "閏年です。";
+            }
+            else
+            {
+                tbLeapYear.Text = "閏年ではありません。";
+            }
+#endif
+        }
+
+        private void btBefore_Click(object sender, EventArgs e)
+        {
+            _now = DateTime.Now;
+
+            tbNowDisp.Text = _now.ToString("F");
+
+            var day = _now.AddMonths(-(int)nudMonthBeforeAfter.Value);
+            day = day.AddDays(-(int)nudDayBeforeAfter.Value);
+            
+            tbBeforeAfterDisp.Text = day.ToString("D");
+
+#if true
+            //参考演算子
+            tbLeapYear.Text = DateTime.IsLeapYear(day.Year) ? "閏年です。" : "閏年ではありません。";
+
+#else
+            if (DateTime.IsLeapYear(day.Year))
+            {
+                tbLeapYear.Text = "閏年です。";
+            }
+            else
+            {
+                tbLeapYear.Text = "閏年ではありません。";
+            }
+#endif
+        }
+
+        private void btAfter_Click(object sender, EventArgs e)
+        {
+            _now = DateTime.Now;
+
+            tbNowDisp.Text = _now.ToString("F");
+
+            var day = _now.AddMonths((int)nudMonthBeforeAfter.Value);
+            day = day.AddDays((int)nudDayBeforeAfter.Value);
+
+            tbBeforeAfterDisp.Text = day.ToString("D");
+
+#if true
+            //三項演算子
+            tbLeapYear.Text = DateTime.IsLeapYear(day.Year) ? "閏年です。" : "閏年ではありません。";
+
+#else
+            if (DateTime.IsLeapYear(day.Year))
+            {
+                tbLeapYear.Text = "閏年です。";
+            }
+            else
+            {
+                tbLeapYear.Text = "閏年ではありません。";
+            }
+#endif
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _now = DateTime.Now;
+
+            var day = new DateTime((int)nudYear.Value, (int)nudMonth.Value, (int)nudDay.Value);
+
+            int interval = (int)(_now - day).TotalDays;
+
+            tbAns.Text = interval.ToString();
+        }
+    }
+}
